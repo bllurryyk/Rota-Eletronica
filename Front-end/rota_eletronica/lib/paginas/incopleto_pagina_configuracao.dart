ce385296443e6491b863ff0a4edf755f2037ca62
vitorcosta26 <60762180+vitorcosta26@users.noreply.github.com>
2023-10-21 22:06:11 -0300

    feat(RE-14): desenvolvendo tela de configurações

--- Front-end/rota_eletronica/lib/paginas/incopleto_pagina_configuracao.dart
+++ Front-end/rota_eletronica/lib/paginas/incopleto_pagina_configuracao.dart
@@ -4,6 +4,27 @@ import 'package:flutter/material.dart';
 import 'package:rota_eletronica/paginas/pagina_onboarding.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rota Eletrônica',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const PaginaMinhaConta(),
+    );
+  }
+}
+
 class PaginaMinhaConta extends StatefulWidget {
   const PaginaMinhaConta({
     super.key,
@@ -16,34 +37,95 @@ class PaginaMinhaConta extends StatefulWidget {
 class _PaginaMinhaContaState extends State<PaginaMinhaConta> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Center(
+    return Scaffold(
+      appBar: AppBar(actions: [
+        const Spacer(),
+        IconButton(
+          padding: const EdgeInsets.all(16.0),
+          onPressed: () async {
+            final preferencia = await SharedPreferences.getInstance();
+            preferencia.setBool('mostrarHome', false);
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => const PaginaOnboarding()));
+          },
+          icon: const Icon(
+            Icons.exit_to_app,
+            color: Color(0xff004088),
+          ),
+        ),
+      ]),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              "Minha Conta",
-              style: TextStyle(color: Color(0xff8497FE)),
-            ),
             Container(
               margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
               width: 150,
               height: 150,
               child: Image.network("lib/image/logo.png"),
             ),
-            TextButton(
-                onPressed: () async {
-                  final preferencia = await SharedPreferences.getInstance();
-                  preferencia.setBool('mostrarHome', false);
-                  Navigator.of(context).pushReplacement(MaterialPageRoute(
-                      builder: (context) => const PaginaOnboarding()));
-                },
-                child: const Text(
-                  'Sair',
-                  style: TextStyle(color: Color(0xff8497FE)),
-                ))
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: const Column(
+                children: [
+                  SizedBox(height: 35),
+                  WidgetBotao(texto: 'Editar cadastro'),
+                  SizedBox(height: 10),
+                  WidgetBotao(texto: 'Ajuda'),
+                  SizedBox(height: 10),
+                  WidgetBotao(texto: 'Sobre o projeto'),
+                  SizedBox(height: 10),
+                  WidgetBotao(texto: 'Entre em contato conosco'),
+                  SizedBox(height: 50),
+                ],
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class WidgetBotao extends StatefulWidget {
+  final String texto;
+
+  const WidgetBotao({
+    super.key,
+    required this.texto,
+  });
+
+  @override
+  State<WidgetBotao> createState() => _WidgetBotaoState();
+}
+
+class _WidgetBotaoState extends State<WidgetBotao> {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size.fromHeight(50),
+        backgroundColor: const Color(0xffEFF2FF),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Text(
+            widget.texto,
+            style: const TextStyle(
+              color: Color(0xff8497FE),
+            ),
+          ),
+          const Spacer(),
+          const Icon(
+            Icons.arrow_forward,
+            color: Color(0xff8497FE),
+          ),
+        ],
       ),
     );
   }
