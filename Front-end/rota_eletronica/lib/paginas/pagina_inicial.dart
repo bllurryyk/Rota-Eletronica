b3d2019911440f125ed4be4f1e8c20eb3893d319
vitorcosta26 <60762180+vitorcosta26@users.noreply.github.com>
2023-10-29 12:01:46 -0300

    feat(RE-26): desenvolvendo tela inical/pesquisa

--- Front-end/rota_eletronica/lib/paginas/pagina_inicial.dart
+++ Front-end/rota_eletronica/lib/paginas/pagina_inicial.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rota_eletronica/testes/busca.dart';
 
 class PaginaInicial extends StatefulWidget {
   const PaginaInicial({Key? key}) : super(key: key);
@@ -111,41 +112,37 @@ class Pesquisa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(
-          top: 15.0, left: 25.0, right: 25.0, bottom: 15.0),
-      child: Container(
-        decoration: BoxDecoration(boxShadow: [
-          BoxShadow(
-              offset: const Offset(12, 26),
-              blurRadius: 50,
-              spreadRadius: 0,
-              color: Colors.grey.withOpacity(.1)),
-        ]),
-        child: TextField(
-          onChanged: (value) {
-            //Do something
-          },
-          decoration: const InputDecoration(
-            prefixIcon: Icon(Icons.search),
-            filled: true,
-            fillColor: Colors.white,
-            hintText: 'O que deseja descartar?',
-            hintStyle: TextStyle(color: Colors.grey),
-            contentPadding:
-                EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(15.0)),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white, width: 1.0),
-              borderRadius: BorderRadius.all(Radius.circular(15.0)),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.white, width: 2.0),
-              borderRadius: BorderRadius.all(Radius.circular(15.0)),
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: const Color(0xffF2F2F2)),
+                  borderRadius: BorderRadius.circular(12)),
+              child: TextField(
+                onTap: () {
+                  showSearch(
+                      context: context, delegate: CustomSearchDelegate());
+                },
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  border: InputBorder.none,
+                  hintText: 'O que deseja descartar?',
+                  hintStyle: TextStyle(color: Colors.grey),
+                ),
+              ),
             ),
           ),
-        ),
+          const SizedBox(width: 12.0),
+          Container(
+            padding: const EdgeInsets.all(12.0),
+            decoration: BoxDecoration(
+                border: Border.all(color: const Color(0xffF2F2F2)),
+                borderRadius: BorderRadius.circular(12)),
+            child: const Icon(Icons.filter_alt_outlined),
+          ),
+        ],
       ),
     );
   }
