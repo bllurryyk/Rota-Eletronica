6e5c4e3cf41bdb909d35f40d494a7f96ad50ce07
vitorcosta26 <60762180+vitorcosta26@users.noreply.github.com>
2023-11-20 15:59:53 -0300

    Corrigindo posição atual do usuário

--- Front-end/rota_eletronica/lib/paginas/pagina_mapa.dart
+++ Front-end/rota_eletronica/lib/paginas/pagina_mapa.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:rota_eletronica/paginas/busca.dart';
 import 'package:rota_eletronica/paginas/pagina_empresa.dart';
@@ -19,18 +20,29 @@ class PaginaMapa extends StatefulWidget {
 
 class _PaginaMapaState extends State<PaginaMapa> {
   LatLng posicaoAtual = aracaju;
+  late double latitude;
+  late double longitude;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Stack(
         children: [
-          const Mapa(),
+          Mapa(
+            posicaoAtual: posicaoAtual,
+          ),
           Positioned(
             bottom: 5.0,
             right: 5.0,
             child: InkWell(
-              onTap: () {},
+              onTap: () {
+                _getCurrentLocation().then((value) =>
+                    {latitude = value.latitude, longitude = value.longitude});
+                setState(() {
+                  posicaoAtual = LatLng(latitude, longitude);
+                });
+                _liveLocation();
+              },
               child: Container(
                 height: 40,
                 width: 40,
@@ -49,6 +61,42 @@ class _PaginaMapaState extends State<PaginaMapa> {
         ],
       ),
     );
+  }
+
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permission');
+    }
+
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
+  void _liveLocation() {
+    LocationSettings locationSettings = const LocationSettings(
+        accuracy: LocationAccuracy.high, distanceFilter: 50);
+
+    Geolocator.getPositionStream(locationSettings: locationSettings)
+        .listen((Position position) {
+      latitude = position.latitude;
+      longitude = position.longitude;
+
+      setState(() {
+        posicaoAtual = LatLng(latitude, longitude);
+      });
+    });
   }
 }
 
@@ -126,7 +174,9 @@ class _WidgetBotaoCabecalhoState extends State<WidgetBotaoCabecalho> {
 }
 
 class Mapa extends StatefulWidget {
-  const Mapa({super.key});
+  const Mapa({super.key, required this.posicaoAtual});
+
+  final LatLng posicaoAtual;
 
   @override
   State<Mapa> createState() => _MapaState();
@@ -136,8 +186,8 @@ class _MapaState extends State<Mapa> {
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
-      options: const MapOptions(
-        initialCenter: aracaju,
+      options: MapOptions(
+        initialCenter: widget.posicaoAtual,
         initialZoom: 13.0,
       ),
       children: [
@@ -162,16 +212,18 @@ class _MapaState extends State<Mapa> {
                       useSafeArea: true,
                       context: context,
                       builder: (context) {
-                        return const Wrap(
+                        return Wrap(
                           crossAxisAlignment: WrapCrossAlignment.center,
                           children: [
                             Padding(
-                              padding: EdgeInsets.only(
+                              padding: const EdgeInsets.only(
                                   bottom: 70.0, left: 50.0, right: 50.0),
                               child: WidgetEmpresa(
-                                  nome: 'Eco TI Ambiental',
-                                  endereco:
-                                      'Av. Coelho e Campos, 784 - Centro, Aracaju - SE, 49060-000'),
+                                nome: 'Eco TI Ambiental',
+                                endereco:
+                                    'Av. Coelho e Campos, 784 - Centro, Aracaju - SE, 49060-000',
+                                onPressed: () {},
+                              ),
                             ),
                           ],
                         );
@@ -180,6 +232,13 @@ class _MapaState extends State<Mapa> {
                   },
                   child:
                       ClipRRect(child: Image.asset('assets/images/pin.png')))),
+          Marker(
+              point: widget.posicaoAtual,
+              child: const Icon(
+                Icons.circle,
+                color: Colors.purple,
+                size: 10,
+              )),
         ])
       ],
     );
@@ -189,11 +248,13 @@ class _MapaState extends State<Mapa> {
 class WidgetEmpresa extends StatefulWidget {
   final String nome;
   final String endereco;
+  final Function onPressed;
 
   const WidgetEmpresa({
     super.key,
     required this.nome,
     required this.endereco,
+    required this.onPressed,
   });
 
   @override
@@ -232,7 +293,9 @@ class _WidgetEmpresaState extends State<WidgetEmpresa> {
         ),
         subtitle: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
           TextButton.icon(
-              onPressed: () {},
+              onPressed: () {
+                widget.onPressed;
+              },
               icon: const Icon(
                 Icons.add,
                 color: Color(0xff999999),
